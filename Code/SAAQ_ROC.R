b32742bfefc01f2d348e25af6d71141dcdc4d104
Lee Morin <42477710+LeeMorinUCF@users.noreply.github.com>
2021-06-24 17:08:09 -0400

    Calculation of ROC curve

--- Code/SAAQ_ROC.R
+++ Code/SAAQ_ROC.R
@@ -5,6 +5,23 @@ library(data.table)
 
 # Calculations using rank.
 
+# # Direct calculation:
+# 
+# auroc_dt <- saaq_data[, c('events', 'fit', 'num')]
+# 
+# summary(auroc_dt)
+# 
+# # Take the cumulative sum of ranked predictions, weighted by events.
+# # n1 =  sum(lr$y) 
+# # auroc = data.table(y=lr$y , p=lr$fitted.values)[, rp := frank(p)][y==1, (sum(rp)-.5*n1*(n1+1))/(n1*(n-n1))]  ## area under ROC curve = Mann-Whitney-Wilcoxon statistic
+# 
+# num_sum <- auroc_dt[, sum(num)]
+# num_1 <- auroc_dt[events == 1, sum(num)]
+# auroc_dt[, rank := frank(fit)]
+# auroc_dt[events == 1, (sum(rank*num) - num_1*(num_1+1)/2)/(num_1*(num_sum - num_1))]
+
+
+
 test_auroc <- data.table(fit = 10 - seq(10), 
                          events = c(1,1,0,1,1,1,0,0,1,0), 
                          num = rep(1, 10))
@@ -107,6 +124,19 @@ roc <- roc.curve(scores.class0 = x_0, scores.class1 = x_1,
                  weights.class0 = w_0, weights.class1 = w_1, curve = TRUE )
 roc
 plot(roc)
+
+# Use negative scores if order is wrong.
+# Scores in order increasing with class 1.
+x_0 <- test_auroc_agg_1[y == 0, -x1]
+x_1 <- test_auroc_agg_1[y == 1, -x1]
+w_0 <- test_auroc_agg_1[y == 0, N]
+w_1 <- test_auroc_agg_1[y == 1, N]
+roc <- roc.curve(scores.class0 = x_0, scores.class1 = x_1, 
+                 weights.class0 = w_0, weights.class1 = w_1, curve = TRUE )
+roc
+plot(roc)
+
+
 
 
 # Tests pass:
